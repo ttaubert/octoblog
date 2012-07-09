45012a21227d38cd9c8fa4e1e1dee456112c1aa3
Tim Taubert <tim@timtaubert.de>
2012-07-09 22:19:15 +0200

    added lightbox

---
+++ minify.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# app
+cat scripts/jquery.min.js \
+    scripts/slimbox2.js \
+    > source/javascripts/main.tmp.js
+
+java -jar ~/Downloads/yuicompressor-2.4.7.jar \
+    -o source/javascripts/main.js \
+    source/javascripts/main.tmp.js
+
+rm source/javascripts/main.tmp.js
